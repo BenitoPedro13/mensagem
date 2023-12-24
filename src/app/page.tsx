1c6124ea5a8ff2d6a75f553e258ede0eb4e3d350
Benito <benitopedro21@outlook.com>
2023-12-24 10:33:52 -0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,83 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+        <p className="fixed left-0 top-0 flex w-full items-center justify-between border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          E aiii, Tudo bem ? <span className="text-lg ml-3">😅</span>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="pointer-events-none cursor-pointer flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             target="_blank"
             rel="noopener noreferrer"
+            onClick={(e) => router.push("/voce-clicou-no-memoji")}
           >
-            By{' '}
+            Nao clique nesse memoji!!!!!{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
+              src="/memoji.png"
+              alt="Benito Pedro"
+              width={120}
+              height={120}
               priority
             />
           </a>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="flex flex-col place-items-center">
+        <p>Oiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii :D</p>
+        <br />
+        <p>Como voce esta ? espero que melhor !!!!!</p>
+        <br />
+        <p>Nao consigo entender pq eu nao posso mais falar com voce :(</p>
+        <br />
+        <p>Eu estou com medo de voce nao me desbloquear mais...</p>
+        <p>
+          tudo bem se nao desbloquear tbm, mas eu to sentindo falta de conversar
+          com voce
+        </p>
+        <br />
+        <p>
+          se o problema for pq voce namora, eu paro de ficar flertando com voce
+          e podemos ser amigos
+        </p>
+        <p>
+          eu so queria sua amizade.. é tao dificil achar pessoas parecidas com
+          voce
+        </p>
+        <br />
+        <p>
+          se eu te tratar apenas como amiga, a gente pode voltar a se falar ?
+          prometo nunca mais tentar ficar com voce
+        </p>
+        <br />
+        <p>
+          Ontem meu dia foi horrivel, muito tempo que nao me sentia mal assim,
+          mas hoje ja estou bem melhor :)
+        </p>
+        <br />
+        <p>
+          enfim, e isso... desculpa novamente por complicar mais sua vida do que
+          ela ja é
+        </p>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          href="https://wa.me/5521983849852"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <button className="flex items-center w-[250px] mt-6 cursor-pointer rounded-lg transition-all h-16 font-bold text-base bg-[#04FF93] hover:scale-110 text-black shadow-md px-6 py-4 border-2 border-black">
+            Vire minha amiga!! <span className="text-2xl ml-3">🥹</span>
+          </button>
         </a>
       </div>
     </main>
-  )
+  );
 }
+//
---
+++ src/app/voce-clicou-no-memoji/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <img src="https://media1.giphy.com/media/xTkcEW2Oj04bLsGoyk/giphy.gif?cid=ecf05e47trbrunczucbrvt7t02iw8793q7tdxd9xou52948u&ep=v1_gifs_search&rid=giphy.gif&ct=g" />
+    </div>
+  );
+};
+
+export default page;
